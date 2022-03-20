f2f8e77b3a532b2c61380ec5693ec620872cafcd
Laddge <dev.laddge@gmail.com>
2022-03-20 11:37:36 +0000

    [feat] add main class

--- tanuky/__init__.py
+++ tanuky/__init__.py
@@ -1,3 +1,3 @@
 from .tanuky import *
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
--- tanuky/tanuky.py
+++ tanuky/tanuky.py
@@ -10,3 +10,10 @@ class MdDoc:
         self.config = yaml.safe_load(yml)
 
         self.body = s.split("---")[2].strip()
+
+
+class Tanuky:
+    def __init__(self, srcdir="./src", tpldir="./template", distdir="./dist"):
+        self.srcdir = srcdir
+        self.tpldir = tpldir
+        self.distdir = distdir
