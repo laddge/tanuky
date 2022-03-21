ae99bccbd7843fd91d5b341c873c6134fd886ec4
Laddge <dev.laddge@gmail.com>
2022-03-21 01:17:29 +0000

    [feat] add generate func

--- tanuky/tanuky.py
+++ tanuky/tanuky.py
@@ -1,5 +1,15 @@
+import os
+import shutil
+import re
+import tempfile
+import glob
 import yaml
 import markdown
+import jinja2
+
+
+class RenderingErr(Exception):
+    pass
 
 
 class MdDoc:
@@ -21,3 +31,33 @@ class Tanuky:
 
     def mkhtml(self, mdbody):
         return markdown.markdown(mdbody)
+
+    def generate(self):
+        files = glob.glob(os.path.join(self.srcdir, "**"), recursive=True)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            for path in files:
+                saveto = os.path.join(tmpdir, re.sub(f"^{self.srcdir}/?", "", path))
+                os.makedirs(os.path.dirname(saveto), exist_ok=True)
+                if path[-3:] == ".md":
+                    doc = MdDoc(path)
+                    config = doc.config
+                    config["Body"] = self.mkhtml(doc.body)
+
+                    if "Template" not in config.keys():
+                        raise RenderingErr("No template specified")
+                    tplpath = os.path.join(self.tpldir, doc.config["Template"] + ".html")
+                    if not os.path.exists(tplpath):
+                        raise RenderingErr("Template not found")
+                    with open(tplpath) as f:
+                        tpl = jinja2.Template(f.read())
+
+                    saveto = saveto[:-3] + ".html"
+                    with open(saveto, "w") as f:
+                        f.write(tpl.render(config))
+                else:
+                    shutil.copy(path, saveto)
+            if os.path.isfile(self.distdir):
+                os.remove(self.distdir)
+            if os.path.isdir(self.distdir):
+                shutil.rmtree(self.distdir)
+            shutil.copytree(tmpdir, self.distdir)
